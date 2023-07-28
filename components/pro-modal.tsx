6e91dc3de90f20924e727f765e667ca940f0bbe6
DigitalPotential <Carlos@digitalpotential.se>
2023-07-29 01:56:48 +0200

    feat: adding pro modal/hook

---
+++ components/pro-modal.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import { useProModal } from "@/hooks/use-pro-modal";
+import { Badge } from "@/components/ui/badge";
+
+export const ProModal = () => {
+    const proModal = useProModal();
+    return (
+        <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle className="flex justify-center items-center flex-col gap-y-4 pb-2">
+                       <div className="flex items-center gap-x-2 font-bold py-1">
+                       Upgrade to Pro Plan
+                        <Badge className="uppercase text-sm py-1">
+                            pro
+                        </Badge>
+                       </div>
+                    </DialogTitle>
+                </DialogHeader>
+            </DialogContent>
+        </Dialog>
+    )
+}
