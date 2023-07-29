ebdf7f24423e2660833aaaf04bba6a2fb2b5f528
DigitalPotential <Carlos@digitalpotential.se>
2023-07-30 00:22:10 +0200

    feat: check subscription

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import axios from "axios";
+import { useState } from "react";
 import { MessageSquare, Music, VideoIcon, ImageIcon, Code, Check, Zap } from "lucide-react";
 
 
@@ -45,6 +47,21 @@ const tools = [
 
 export const ProModal = () => {
     const proModal = useProModal();
+    const [loading, setLoading] = useState(false);
+
+    const onSubscribe = async () => {
+        try {
+            setLoading(true);
+            const response = await axios.get("/api/stripe")
+
+            window.location.href = response.data.url;
+        } catch (error) {
+         console.log(error, "STRIPE_CLIENT_ERROR");   
+        } finally {
+            setLoading(false);
+        }
+    }
+
     return (
         <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
             <DialogContent>
@@ -78,6 +95,7 @@ export const ProModal = () => {
                 </DialogHeader>
                 <DialogFooter>
                             <Button
+                                onClick={onSubscribe}
                                 size="lg"
                                 variant="premium"
                                 className="w-full"
